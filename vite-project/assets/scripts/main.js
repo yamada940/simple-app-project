17c170b58bb3bd7696e21f8e544a311ee85efe09
seito <seito.horiguchi@gmail.com>
2022-12-31 17:03:25 +0900

    slider

---
+++ vite-project/assets/scripts/main.js
@@ -0,0 +1,8 @@
+import '../styles/main.scss'
+import '@splidejs/splide/css';
+import '@splidejs/splide/css/core';
+import '@splidejs/splide/css/skyblue';
+import Splide from '@splidejs/splide';
+
+new Splide( '.splide' ).mount();
+// import { setupCounter } from '../../vite-project/counter.js'
--- vite-project/main.js
+++
@@ -1,3 +0,0 @@
-import '../assets/styles/main.scss'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
