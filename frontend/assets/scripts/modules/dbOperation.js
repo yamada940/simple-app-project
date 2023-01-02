945126e920a251f38dd17488b3d845caa38e02e3
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-01-02 20:20:44 +0900

    fixed firebase issue

--- frontend/assets/scripts/modules/dbOperation.js
+++ frontend/assets/scripts/modules/dbOperation.js
@@ -1,32 +1,45 @@
-import firebase from "firebase/compat/app";
-// import { getFirestore } from "firebase/compat/firestore";
-import "firebase/compat/firestore";
+// import * as firebase from "firebase/app";
+import firebase from 'firebase/compat/app';
+// import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 
 export function dbOperation(){
+    // TODO: Replace the following with your app's Firebase project configuration
+    // See: https://firebase.google.com/docs/web/learn-more#config-object
+    const firebaseConfig = {
+        apiKey: "AIzaSyCeiAgerHoiqBMmJQyIwWTlaQIHs3VJfRM",
+        authDomain: "db-test-6a682.firebaseapp.com",
+        projectId: "db-test-6a682",
+        storageBucket: "db-test-6a682.appspot.com",
+        messagingSenderId: "747176992339",
+        appId: "1:747176992339:web:2b886d51f1151a8a2ab0a6",
+        measurementId: "G-S8L5L31DJY"
+    };
+    
+    // Initialize Firebase
+    const firebaseApp = firebase.initializeApp(firebaseConfig);
+    // const db = firebase.firestore();
+    const db = getFirestore(firebaseApp);
 
-    const init = () => {
-        // TODO: Replace the following with your app's Firebase project configuration
-        // See: https://firebase.google.com/docs/web/learn-more#config-object
-        const firebaseConfig = {
-            apiKey: "AIzaSyCeiAgerHoiqBMmJQyIwWTlaQIHs3VJfRM",
-            authDomain: "db-test-6a682.firebaseapp.com",
-            projectId: "db-test-6a682",
-            storageBucket: "db-test-6a682.appspot.com",
-            messagingSenderId: "747176992339",
-            appId: "1:747176992339:web:2b886d51f1151a8a2ab0a6",
-            measurementId: "G-S8L5L31DJY"
-        };
+    const showData = () => {
+        
+        const getData = async () => {
+            // const q = query(collection(db, "users"), where("capital", "==", true));
+            const q = query(collection(db, "users"));
 
-        // Initialize Firebase
-        const app = firebase.initializeApp(firebaseConfig);
-        // const analytics = getAnalytics(app);
+            const querySnapshot = await getDocs(q);
+            querySnapshot.forEach((doc) => {
+                // doc.data() is never undefined for query doc snapshots
+                console.log(doc.id, " => ", doc.data());
+            });
+        }
+        getData();
     }
-    init()
+    showData();
 
     const insertNewUser = (params) => {
         // Initialize Cloud Firestore and get a reference to the service
-        const db = firebase.firestore();
-
+        
         db.collection("users").add({
             first: params.first,
             last: params.last,
