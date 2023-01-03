27e4396326fd70c98d542948a744b8c9b7dc8aeb
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-01-03 17:33:52 +0900

    split to sb-table

--- frontend/assets/scripts/modules/dbOperation.js
+++ frontend/assets/scripts/modules/dbOperation.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase/compat/app';
 import { getFirestore, collection, query, where, getDocs } from "firebase/firestore";
 
+
+// dotenv.config()
 // obj: {
 //  dbName: str 'users'
 //  tableId: str 'js-firebase-table'
@@ -9,12 +11,12 @@ import { getFirestore, collection, query, where, getDocs } from "firebase/firest
 export default class FirebaseDB {
 	constructor(obj){
 		const firebaseConfig = {
-            apiKey: "AIzaSyCeiAgerHoiqBMmJQyIwWTlaQIHs3VJfRM",
+            apiKey: import.meta.env.API_KEY,
+            appId: import.meta.env.APP_ID,
             authDomain: "db-test-6a682.firebaseapp.com",
             projectId: "db-test-6a682",
             storageBucket: "db-test-6a682.appspot.com",
             messagingSenderId: "747176992339",
-            appId: "1:747176992339:web:2b886d51f1151a8a2ab0a6",
             measurementId: "G-S8L5L31DJY"
         };
 
