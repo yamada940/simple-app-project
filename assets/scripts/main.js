7e28f97323520e86c320affdb613d9a4fdaa20cb
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-01-04 16:11:26 +0900

    remove

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,8 @@
 import '../styles/main.scss'
-import '@splidejs/splide/css';
-import '@splidejs/splide/css/core';
-import '@splidejs/splide/css/skyblue';
-import Splide from '@splidejs/splide';
 import ActiveNav from './modules/activeNav';
 
 if(document.getElementById('js-nav') !== null){
 	new ActiveNav({
 		navElement: 'js-nav'
 	})
-}
-
-if(document.getElementsByClassName('splide')[0]){
-	new Splide( '.splide' ).mount();
 }
