82aea676d4b1b8dcde1b6b0f75c98950064c54e0
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-01-04 15:52:35 +0900

    correct env

--- assets/scripts/modules/const.js
+++ assets/scripts/modules/const.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: import.meta.env.API_KEY,
-    appId: import.meta.env.APP_ID,
+    apiKey: import.meta.env.VITE_API_KEY,
+    appId: import.meta.env.VITE_APP_ID,
     authDomain: "db-test-6a682.firebaseapp.com",
-    projectId: "db-test-6a682",
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
     storageBucket: "db-test-6a682.appspot.com",
     messagingSenderId: "747176992339",
     measurementId: "G-S8L5L31DJY"
