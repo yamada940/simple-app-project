2d2780d62550432cbb0c04ab2633410e720888f5
Seito Horiguchi <seito.horiguchi@gmail.com>
2023-01-04 16:03:59 +0900

    revers to env

--- assets/scripts/modules/const.js
+++ assets/scripts/modules/const.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: import.meta.env.VITE_API_KEY,
-    appId: import.meta.env.VITE_APP_ID,
+    apiKey: "AIzaSyCeiAgerHoiqBMmJQyIwWTlaQIHs3VJfRM",
+    appId: "1:747176992339:web:2b886d51f1151a8a2ab0a6",
     authDomain: "db-test-6a682.firebaseapp.com",
-    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    projectId: "db-test-6a682",
     storageBucket: "db-test-6a682.appspot.com",
     messagingSenderId: "747176992339",
     measurementId: "G-S8L5L31DJY"
